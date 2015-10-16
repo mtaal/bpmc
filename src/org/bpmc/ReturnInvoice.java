16cc5e7d15007748254e9af8f1d2b7bd721e9c27
Martin Taal <mtaal@elver.org>
2015-10-16 15:41:32 +0200

    Changed

---
+++ src/org/bpmc/ReturnInvoice.java
@@ -0,0 +1,97 @@
+/*
+ *************************************************************************
+ * The contents of this file are subject to the Openbravo  Public  License
+ * Version  1.1  (the  "License"),  being   the  Mozilla   Public  License
+ * Version 1.1  with a permitted attribution clause; you may not  use this
+ * file except in compliance with the License. You  may  obtain  a copy of
+ * the License at http://www.openbravo.com/legal/license.html
+ * Software distributed under the License  is  distributed  on  an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific  language  governing  rights  and  limitations
+ * under the License.
+ * The Original Code is Openbravo ERP.
+ * The Initial Developer of the Original Code is Openbravo SLU
+ * All portions are Copyright (C) 2015 Openbravo SLU
+ * All Rights Reserved.
+ * Contributor(s):  ______________________________________.
+ ************************************************************************
+ */
+package org.bpmc;
+
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONObject;
+import org.openbravo.client.application.process.BaseProcessActionHandler;
+import org.openbravo.dal.service.OBDal;
+import org.openbravo.erpCommon.utility.OBMessageUtils;
+import org.openbravo.service.db.DbUtility;
+
+public class ReturnInvoice extends BaseProcessActionHandler {
+  private static final Logger log = Logger.getLogger(ReturnInvoice.class);
+
+  @Override
+  protected JSONObject doExecute(Map<String, Object> parameters, String data) {
+    try {
+      final JSONObject jsonData = new JSONObject(data);
+      final String tenderId = jsonData.getString("Bpmc_Tender_ID");
+      Tender tender = OBDal.getInstance().get(Tender.class, tenderId);
+      tender.setTenderstatus("Send Out");
+      // Success Message
+      return getSuccessMessage("Success");
+    } catch (Exception e) {
+      OBDal.getInstance().rollbackAndClose();
+      log.error("Exception creating multiple transactions from payments", e);
+
+      try {
+        Throwable ex = DbUtility.getUnderlyingSQLException(e);
+        String message = OBMessageUtils.translateError(ex.getMessage()).getMessage();
+        return getErrorMessage(message);
+      } catch (Exception ignore) {
+      }
+    }
+
+    return new JSONObject();
+  }
+
+  /**
+   * Returns a JSONObject with the success message to be printed
+   */
+  private static JSONObject getSuccessMessage(final String msgText) {
+    final JSONObject result = new JSONObject();
+    try {
+      final JSONArray actions = new JSONArray();
+      final JSONObject msgInBPTab = new JSONObject();
+      msgInBPTab.put("msgType", "success");
+      msgInBPTab.put("msgTitle", OBMessageUtils.messageBD("success"));
+      msgInBPTab.put("msgText", msgText);
+      final JSONObject msgInBPTabAction = new JSONObject();
+      msgInBPTabAction.put("showMsgInProcessView", msgInBPTab);
+      actions.put(msgInBPTabAction);
+      result.put("responseActions", actions);
+    } catch (Exception e) {
+      log.error(e);
+    }
+
+    return result;
+  }
+
+  /**
+   * Returns a JSONObject with the error message to be printed and retry execution
+   */
+  private static JSONObject getErrorMessage(final String msgText) {
+    final JSONObject result = new JSONObject();
+    try {
+      final JSONObject msg = new JSONObject();
+      msg.put("severity", "error");
+      msg.put("text", msgText);
+      result.put("message", msg);
+      result.put("retryExecution", true);
+    } catch (Exception e) {
+      log.error(e);
+    }
+    return result;
+  }
+
+}
